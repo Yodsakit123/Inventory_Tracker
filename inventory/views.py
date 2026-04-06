95364af50ecc8288a0d8eed8fff6293822826cd4
Yodsakit <Yodsakit.123@gmail.com>
2026-04-06 18:21:52 +0700

    feat: integrate django-plotly-dash to add dynamic production dashboard functionality

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import Part
 
+# Import dash apps to register them with django-plotly-dash
+from . import dash_apps
+
 def part_list(request):
     parts = Part.objects.all()
     return render(request, 'inventory/part_list.html', {'parts': parts})
+
+def dashboard(request):
+    return render(request, 'inventory/dashboard.html')
